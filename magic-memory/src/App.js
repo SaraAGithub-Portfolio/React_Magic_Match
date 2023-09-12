e565f9380bd60fb7fd571f990cf65d37ab6214eb
SaraAGithub-Portfolio <skausa29@gmail.com>
2023-09-12 18:44:25 +0100

    added finishing touches. Game is fully functional now.

--- magic-memory/src/App.js
+++ magic-memory/src/App.js
@@ -17,17 +17,18 @@ function App() {
   const [turns, setTurns] = useState(0)
   const [choiceOne, setChoiceOne] = useState(null)
   const [choiceTwo, setChoiceTwo] = useState(null)
+  const [disabled, setDisabled] = useState(false)
 
   //shuffle cards
   const shuffleCards = () => {
     const shuffledCards = [...cardImages, ...cardImages]
       .sort(() => Math.random() - 0.5)
       .map((card) => ({ ...card, id: Math.random() }))
+    setChoiceOne(null)
+    setChoiceTwo(null)
     setCards(shuffledCards)
     setTurns(0)
   }
-
-
   //handle a choice
   const handleChoice = (card) => {
     choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
@@ -35,7 +36,9 @@ function App() {
 
   //compare 2 selected cards
   useEffect(() => {
+
     if (choiceOne && choiceTwo) {
+      setDisabled(true)
       if (choiceOne.src === choiceTwo.src) {
         setCards(prevCards => {
           return prevCards.map(card => {
@@ -49,7 +52,7 @@ function App() {
         resetTurn();
       } else {
 
-        resetTurn();
+        setTimeout(() => resetTurn(), 1000)
       }
     }
   }, [choiceOne, choiceTwo])
@@ -60,7 +63,12 @@ function App() {
     setChoiceOne(null)
     setChoiceTwo(null)
     setTurns(prevTurns => prevTurns + 1)
+    setDisabled(false)
   }
+
+  useEffect(() => {
+    shuffleCards();
+  }, [])
 
   return (
     <div className="App">
@@ -73,9 +81,11 @@ function App() {
             card={card}
             handleChoice={handleChoice}
             flipped={card === choiceOne || card === choiceTwo || card.matched}
+            disabled={disabled}
           />
         ))}
       </div>
+      <p>Turns:{turns}</p>
     </div >
   );
 }
